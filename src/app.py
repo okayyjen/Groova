22a26b9d0bccea9cb39972fe27901ca4ae3a8310
Amirah <harbamirah@gmail.com>
2023-09-07 19:25:33 -0300

    login/logout function complete

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, url_for,session, redirect, render_template
+from flask_session import Session
 import spotipy 
 from spotipy.oauth2 import SpotifyOAuth
 import os
@@ -14,12 +15,30 @@ clientSecret = os.getenv("CLIENT_SECRET")
 app = Flask(__name__)
 
 app.sercret_key = "AKjhnd79Huha"
-app.config['SESSION_COOKIE_NAME'] = 'cookie dookie'
-
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+Session(app)
 
 @app.route('/')
-def landingPage():
-    return render_template("index.html")
+def index():
+
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private',
+                                               cache_handler=cache_handler,
+                                               show_dialog=True)
+    
+    if request.args.get("code"):
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect('/')
+    
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        auth_url = auth_manager.get_authorize_url()
+        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+        
+
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return render_template("main.html")
 
 @app.route('/login')
 def login():
@@ -29,7 +48,8 @@ def login():
 
 @app.route('/redirect')
 def redirectPage():
-    return 'redirect'
+
+    return render_template("main.html")
 
 @app.route('/getTracks')
 def getTracks():
@@ -41,3 +61,11 @@ def create_spotify_oauth():
             client_secret=clientSecret,
             redirect_uri=url_for('redirectPage', _external=True),
             scope="user-library-read")
+
+@app.route('/logout')
+def logout():
+    
+    for key in list(session.keys()):
+        session.pop(key)
+        
+    return redirect('/')
