0202fad4b5ebe69086b6005f77e1d5e5c555b229
Amirah <harbamirah@gmail.com>
2023-09-11 19:28:47 -0300

    spotifyTools.py added

---
+++ src/spotifyTools.py
@@ -0,0 +1,13 @@
+import re
+
+def extract(response):
+    pattern = r'(acousticness|danceability|tempo|valence|energy):?\s*(\d+(\.\d+)?)'
+    matches = re.findall(pattern, response)
+    matches_dict = {}
+    for match in matches:
+        keyword, value, _ = match
+        matches_dict[keyword] = float(value)
+    return matches_dict
+
+
+# Test the function with your input string
