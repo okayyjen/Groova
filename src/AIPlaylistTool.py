893f70ca7688cce1c90b67cd7fba3b936cfa63b2
Amirah <harbamirah@gmail.com>
2023-09-22 00:54:06 -0300

    AI function update, adding genres to recommendation seeds

--- src/AIPlaylistTool.py
+++ src/AIPlaylistTool.py
@@ -4,17 +4,17 @@ import os
 import SpotifyTools
 
 class PlaylistTool(BaseTool):
-    """Tool that creates a new playlist and adds tracks to it on Spotify based on the given features."""
+    """Tool that creates a new playlist and adds tracks to it on Spotify based on the given features and genres received on input."""
 
     name = "PlaylistTool"
     description = (
         "Always use this tool when given input"
-        "This tool requires a string representation of musical features as a keyword argument. You will get this from the input given to you. The argument is A STRING"
-        "The features argument is the input you received"
+        "This tool requires a string representation of musical features and genres as one keyword argument. You will get this from the input given to you. The argument is A STRING"
+        "The features_and_genres argument is the input you received"
         "The return statement of this function is the link to the spotify playlist you have created"
     )
 
-    def _run(self, features,*args, **kwargs) -> str:
+    def _run(self, features_and_genres,*args, **kwargs) -> str:
         user = os.getenv('SPOTIFY_USER_ID')
         if not user:
             raise ValueError("SPOTIFY_USER_ID environment variable is not set.")
@@ -24,15 +24,15 @@ class PlaylistTool(BaseTool):
             raise ValueError("SPOTIFY_ACCESS_TOKEN environment variable is not set.")
         
         #convert string dict into dict
-        features_dict = SpotifyTools.extract_and_format(features)
-        
+        features_dict, genre_list = SpotifyTools.extract_and_format(features_and_genres)
+
         sp = spotipy.Spotify(auth=token)
 
         # Create a new empty playlist
         user_playlist = sp.user_playlist_create(user, "New Playlist", public=False, collaborative=False, description="desc")
 
         #getting user's top artists
-        top_artists = sp.current_user_top_artists(time_range='medium_term', limit=5)
+        top_artists = sp.current_user_top_artists(time_range='medium_term', limit=3)
 
         #extracting user's top artists urls
         artist_URLs = []
@@ -42,7 +42,7 @@ class PlaylistTool(BaseTool):
             artist_URLs.append(artist_url)
 
         #get recommended tracks based on users top artists and features
-        recommended_tracks = sp.recommendations(seed_artists=artist_URLs, limit=20, target_features=features_dict)
+        recommended_tracks = sp.recommendations(seed_artists=artist_URLs, seed_genres= genre_list , limit=20, target_features=features_dict)
         recommended_track_uris = [track['uri'] for track in recommended_tracks['tracks']]
         playlist_id = user_playlist['id']
 
@@ -51,7 +51,7 @@ class PlaylistTool(BaseTool):
         playlist_url = user_playlist['external_urls']['spotify']
         return playlist_url
 
-    async def _arun(self, features,*args, **kwargs) -> str:
+    async def _arun(self, features_and_genres,*args, **kwargs) -> str:
         raise NotImplementedError("This tool does not support async")
     
 
--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,7 @@
 CONTENT_CHAIN_1 = """
                         ### Instruction:
                         About you:
-                        You are an AI Agent tasked with rating the type of music a user should listen to based on the user's given situation or mood, using the music attributes provided below.Please provide ratings based on your assessment, and do not rely on the example values I have given you. Do not reply on your past answers.
+                        You are an AI Agent tasked with rating the type of music a user should listen to based on the user's given situation or mood, using the music attributes provided below.Please provide ratings based on your assessment, and do not rely on the example values I have given you. Do not reply on your past answers. You will also suggest 2 genres that fit the situation / mood of the user from the given list below.When picking genres, your goal is to provide genre suggestions that closely match the user's current emotional state / situation.
                         
                         Music attributes:
 
@@ -29,18 +29,127 @@ CONTENT_CHAIN_1 = """
                         
                         --end of things to rate--
 
+                        song genres: 
+
+                        "acoustic",
+                        "afrobeat",
+                        "alt-rock",
+                        "alternative",
+                        "ambient",
+                        "anime",
+                        "black-metal",
+                        "blues",
+                        "bossanova",
+                        "brazil",
+                        "breakbeat",
+                        "british",
+                        "children",
+                        "chill",
+                        "classical",
+                        "club",
+                        "comedy",
+                        "country",
+                        "dance",
+                        "dancehall",
+                        "death-metal",
+                        "deep-house",
+                        "detroit-techno",
+                        "disco",
+                        "disney",
+                        "drum-and-bass",
+                        "dub",
+                        "dubstep",
+                        "edm",
+                        "electro",
+                        "electronic",
+                        "emo",
+                        "folk",
+                        "french",
+                        "funk",
+                        "garage",
+                        "german",
+                        "gospel",
+                        "goth",
+                        "grindcore",
+                        "groove",
+                        "grunge",
+                        "guitar",
+                        "happy",
+                        "hard-rock",
+                        "hardcore",
+                        "heavy-metal",
+                        "hip-hop",
+                        "holidays",
+                        "honky-tonk",
+                        "house",
+                        "idm",
+                        "indian",
+                        "indie",
+                        "indie-pop",
+                        "industrial",
+                        "iranian",
+                        "j-pop",
+                        "j-rock",
+                        "jazz",
+                        "k-pop",
+                        "kids",
+                        "latin",
+                        "metal",
+                        "movies",
+                        "new-age",
+                        "new-release",
+                        "opera",
+                        "party",
+                        "piano",
+                        "pop",
+                        "power-pop",
+                        "punk",
+                        "punk-rock",
+                        "r-n-b",
+                        "rainy-day",
+                        "reggae",
+                        "reggaeton",
+                        "road-trip",
+                        "rock",
+                        "rock-n-roll",
+                        "romance",
+                        "sad",
+                        "salsa",
+                        "samba",
+                        "show-tunes",
+                        "singer-songwriter",
+                        "sleep",
+                        "songwriter",
+                        "soul",
+                        "soundtracks",
+                        "spanish",
+                        "study",
+                        "summer",
+                        "swedish",
+                        "synth-pop",
+                        "tango",
+                        "techno",
+                        "trance",
+                        "trip-hop",
+                        "turkish",
+                        "work-out",
+                        "world-music"
+
+                        ----end of genres to pick from----
+
                         The prompt you will be given is a human's described situation, or mood.
                         
                         RULES:
                         follow the response template below
 
-                        response template (where each "x" should be replaced with your number rating for each corresponding music attribute):
+                        response template (where each "x" should be replaced with your number rating for each corresponding music attribute, and each "y" should be replaced with a genre of your suggestion):
 
                         acousticness: x
                         danceability: x
                         tempo: x
                         valence: x
                         energy: x
+                        genres: y, y
                 
                         ### Prompt:
                         {user_mood}
