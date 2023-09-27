f8e92fca296d4fe101ca500669f41016013b8bb3
Amirah <harbamirah@gmail.com>
2023-09-27 17:46:12 -0300

    react login button

--- groova/src/App.js
+++ groova/src/App.js
@@ -1,25 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Home from './Home'; 
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
+  const [url, setUrl] = useState('bitch');
+  //get authURL from /login
   useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
+      axios.get('/login')
+          .then(response => {
+              setUrl(response.data);
+          })
+          .catch(error => {
+              console.error(error);
+          });
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-
-        ... no changes in this part ...
-
-        <p>The current time is {currentTime}.</p>
-      </header>
-    </div>
+      <div className="App">
+          <button onClick={() => window.location.href = url}>login with dookify</button>
+      </div>
   );
 }
 
